90ed571d3248a20e1f408170d9eb47f16540030c
Bartosz Goik <bartg1999@gmail.com>
2020-07-30 19:58:27 +0200

    Initial version

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args)
         System.out.println("Hello World!")
-        System.out.println("Bartosz")
-        System.out.println("20")
-        System.out.println("G")
+                System.out.println("Bartosz")
+            int("20")
+            char("G")
 }
