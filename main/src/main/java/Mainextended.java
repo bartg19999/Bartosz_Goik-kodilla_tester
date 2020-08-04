d475d13e8ebc04ed1867bf78df351013dd811f0b
Bartosz Goik <bartg1999@gmail.com>
2020-08-04 18:10:38 +0200

    Initial version

---
+++ main/src/main/java/Mainextended.java
@@ -0,0 +1,12 @@
+public class Mainextended {
+    public static void main(String[] args) {
+        String myName = "Bartek";
+        int myAge = 21;
+        double height = 1.80;
+        char drivinglicense = 'B';
+        boolean single = false;
+        String city = "Rybnik";
+        System.out.println(height);
+
+    }
+}
