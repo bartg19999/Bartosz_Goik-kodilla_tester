bb83b67c767b8042e26c3e40c7f8aa858eed6775
Bartosz Goik <bartg1999@gmail.com>
2020-08-09 18:44:14 +0200

    Initial version

--- LeapYear/src/main/java/Leapyear.java
+++ LeapYear/src/main/java/Leapyear.java
@@ -1,18 +1,18 @@
 public class Leapyear {
     public static void main(String[] args) {
-        int year = 2000;
+        int year = 2020;
 
         System.out.println(year);
 
         if (year % 4 == 0) {
             System.out.println("Rok przestępny");
-        } else {
-            System.out.println("Rok nie przestępny");
-        }
-        if (year % 4 == 0 && year % 400 == 0) {
+        } else if (year % 400 == 0 || year % 4 == 0) {
+            System.out.println("Rok przestępny");
+        } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
             System.out.println("Rok przestępny");
         } else {
             System.out.println("Rok nie przestępny");
+
         }
     }
 }
