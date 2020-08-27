d159201ea5de173b1b15446659e8b6376b1bad8a
Bartosz Goik <bartg1999@gmail.com>
2020-08-27 11:32:23 +0100

    Initial version

--- Zadanie2.4/src/main/java/Grades.java
+++ Zadanie2.4/src/main/java/Grades.java
@@ -1,19 +1,29 @@
 public class Grades {
     private int[] grades;
     private int size;
-    private int quantity;
 
     public Grades() {
         this.grades = new int[10];
         this.size = 0;
     }
-    public void add(int grades) {
+    public void add(int value) {
         if (this.size == 10) {
-            this.grades[this.size] = 5;
-            this.size++;
+            return;
         }
+        this.grades[this.size] = value;
+        this.size++;
     }
-    public static void main(String[]args){
-        int grade;
+    public int lastGrade(){
+        if (this.size==0){
+            return 0;
+        }
+    return this.grades[this.size-1];
+    }
+    public int average(){
+        if (this.size == 0) {
+            return 0;
+        }else{
+            return this.grades[this.size/10];
+        }
     }
 }
--- Zadanie2.7/src/main/java/Book.java
+++ Zadanie2.7/src/main/java/Book.java
@@ -1,15 +1,15 @@
+import java.util.Scanner;
+
 public class Book {
+    public static of(String author, String title){
+    public String getAuthor;
+    public String getTitle;}
     private String author;
     private String title;
 
-    public static Book of(String author, String title) {
-        return Book.of(author, title);
+    public static of(String author, String title) {
+        return Book.of("Isaac Asimov", "The Galaxy");
     }
-    public static void main(String[]args){
-        Book book = Book.of("Isaac Asimov", "The Galaxy");
-
-    }
-
 
 }
 
