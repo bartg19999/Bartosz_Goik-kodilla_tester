b2aa7d5aeb6c1806a03bda6e05ec70d59fc2cad2
Bartosz Goik <bartg1999@gmail.com>
2020-08-27 12:07:11 +0100

    Initial version

--- Zadanie2.7/src/main/java/Book.java
+++ Zadanie2.7/src/main/java/Book.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-
 public class Book {
-    public static of(String author, String title){
-    public String getAuthor;
-    public String getTitle;}
     private String author;
     private String title;
 
-    public static of(String author, String title) {
-        return Book.of("Isaac Asimov", "The Galaxy");
+    public static Book of(String author, String title){
+        Book book = new Book();
+        return book;
     }
+    public static void main(String[]args){
+        Book.of("Isaac Asimov", "The Galaxy");
+    }
+
 
 }
 
