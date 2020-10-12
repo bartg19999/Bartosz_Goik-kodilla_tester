7b1be79deef0a7fb5e4d8cab4f945dca59e681e7
Bartosz Goik <bartg1999@gmail.com>
2020-10-12 17:26:47 +0200

    Zadanie 2.5

--- Zadanie2.5/src/main/java/random_numbers/RandomNumbers.java
+++ Zadanie2.5/src/main/java/random_numbers/RandomNumbers.java
@@ -11,22 +11,19 @@ public class RandomNumbers {
 
         int randomNumber = random.nextInt(31);
 
-        int min = 1;
-        int max = 5000;
+        int min;
+        int max = 0;
         int sum = 0;
 
         while (sum <= 5000){
             int n1 = random.nextInt(31);
             sum = sum + n1;
-            System.out.println(n1 + sum);
-        }
-            if (sum < min){
-                {
-                System.out.println(sum + " - minimum");
-            }
-            if (sum > max){
-                System.out.println(sum + " - maximum");
+            System.out.println(sum);
+            if (sum < 30){
+                    System.out.println(sum + " - minimum");
+            }else if (sum > 5000){
+                    System.out.println(sum + " - maximum");
+                }
             }
         }
     }
-}
