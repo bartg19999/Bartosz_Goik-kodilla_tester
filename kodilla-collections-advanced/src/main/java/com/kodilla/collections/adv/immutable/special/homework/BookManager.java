ea012bedc37d4df2c00b90857a3d83f3a83689be
Bartosz Goik <bartg1999@gmail.com>
2020-10-13 20:45:06 +0200

    5.4

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,4 +1,7 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
 public class BookManager {
+    public void createBook(String title, String author) {
+        Book book = new Book(title, author);
+    }
 }
