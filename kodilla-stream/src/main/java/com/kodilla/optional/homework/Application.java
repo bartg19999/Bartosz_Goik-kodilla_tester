24aa520a4a78f6be29b8d710fdddb10493590966
Bartosz Goik <bartg1999@gmail.com>
2020-11-01 11:21:31 +0100

    Zadanie 6.2

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -7,21 +7,18 @@ import java.util.Optional;
 public class Application {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<Student>();
-        students.add(new Student("Tomasz Jagie³³o", new Teacher("Abelard Konieczny")));
+        students.add(new Student("Tomasz Jagiello", new Teacher("Abelard Konieczny")));
         students.add(new Student("Maciej Szybki", null));
-        students.add(new Student("Andrzej Ma³y", new Teacher("Mateusz Nowak")));
+        students.add(new Student("Andrzej Maly", new Teacher("Mateusz Nowak")));
 
         for (Student student : students) {
-            System.out.println("Student: " + student.getName() + " Teacher: " + student.getTeacher());
+            Optional<Student> optionalStudent = Optional.ofNullable(student);
+
+            String message = String.format("Uczen: %s  Nt sauczyciel: %s", optionalStudent.map(Student::getName).orElse("<undefined>"),
+                    optionalStudent.map(Student::getTeacher).map(Teacher::getName).orElse("<undefined>"));
+            System.out.println(message);
         }
 
-        Teacher teacher = null;
-        Optional<Teacher> optionalTeacher = Optional.ofNullable(teacher);
-
-        Teacher name =
-                optionalTeacher.orElse();
-
-        System.out.println(name + "<undefined>");
 
     }
 }
