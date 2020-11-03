cd43e06bcf5ab927d5d1438011faddc7ce06963c
Bartosz Goik <bartg1999@gmail.com>
2020-11-03 17:35:04 +0100

    Zadanie 6.3

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -1,4 +1,26 @@
 package com.kodilla.stream.homework;
 
+import com.kodilla.stream.UsersRepository;
+
 public class ForumStats {
+    public static void main(String[] args) {
+        double avg = UsersRepository.getUserList()
+                .stream()
+                .filter(u -> u.getAge() >= 40)
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(u -> u)
+                .average()
+                .getAsDouble();
+        System.out.println(avg);
+
+        double asd = UsersRepository.getUserList()
+                .stream()
+                .filter(u -> u.getAge() < 40)
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(u -> u)
+                .average()
+                .getAsDouble();
+        System.out.println(asd);
+
+    }
 }
